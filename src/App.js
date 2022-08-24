428996c2db8fad9f88fc88a73ea111b5d2713953
Ludmi Lopez <ludmit@outlook.com>
2022-08-23 21:43:50 -0300

    added-some-links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import React, {useState} from 'react';
-
+import { GrClose } from 'react-icons/gr';
+ 
 const bankOne = [
   {
     keyCode: 81,
@@ -204,12 +205,20 @@ function App() {
     audio.play()
   }
   return (<>
+  <div className='links'>
+    <div className='linked'> <a href='https://www.linkedin.com/in/ludmila-rocio-lopez-082b75218/' target='_blank'><img src='https://res.cloudinary.com/dweiaqd6l/image/upload/v1661301308/imagen_2022-08-23_213507751_nz7ade.png' alt=''></img>
+    <label>Linkedin</label></a> </div>
+    <div className='linked'><a href='https://github.com/DamperDoor56' target='_blank'><img src='https://res.cloudinary.com/dweiaqd6l/image/upload/v1661300500/imagen_2022-08-23_211947424-removebg-preview_qexwa1.png' alt=''></img>
+    <label>Github</label></a></div>
+    <div className='linked'><a href='https://main--personal-portfolio-ludmi-lopez.netlify.app/' target='_blank'><img src='https://res.cloudinary.com/dweiaqd6l/image/upload/v1661301426/imagen_2022-08-23_213626025-removebg-preview_cuzgzu.png' alt=''></img>
+    <label>Portfolio</label></a></div>
+  </div>
   {
     show?<div id='drum-machine'>
     {setKeyVolume()}
     <div className='wrapper'>
-    <div className='navbar-drum'></div>
-
+    <div className='navbar-drum'><i className='icon'><GrClose onClick={() => setShow(!show)}/></i></div>
+    <div className='menus'>
     <div className='first-menu'>
     <Keyboard className='keyboard'
     power={power}
@@ -225,7 +234,9 @@ function App() {
     changeBank={changeBank} name={ soundName || soundsName[soundsType]} />
     </div>
     </div>
+    </div>
     </div>:null
+   
   }
    
 
